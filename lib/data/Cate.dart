3cd87375501a4ea847ba9d38aabbe603c3e1ee4c
mouse <hyeji111544@gmail.com>
2024-10-11 12:41:57 +0900

    feat : 추천 페이지 완료

---
+++ lib/data/Cate.dart
@@ -0,0 +1,33 @@
+class Cate {
+  int categoryId;
+  String categoryName;
+
+  Cate({
+    required this.categoryId,
+    required this.categoryName,
+  });
+}
+
+List<Cate> cateList = [
+  Cate(categoryId: 112011, categoryName: '소설'),
+  Cate(categoryId: 170, categoryName: '경제경영'),
+  Cate(categoryId: 336, categoryName: '자기계발'),
+  Cate(categoryId: 656, categoryName: '인문학'),
+  Cate(categoryId: 798, categoryName: '사회과학'),
+  Cate(categoryId: 74, categoryName: '역사'),
+  Cate(categoryId: 55889, categoryName: '에세이'),
+  Cate(categoryId: 987, categoryName: '과학'),
+  Cate(categoryId: 2913, categoryName: '잡지'),
+  Cate(categoryId: 1230, categoryName: '가정/요리'),
+  Cate(categoryId: 517, categoryName: '예술/대중문화'),
+  Cate(categoryId: 55890, categoryName: '건강/취미'),
+  Cate(categoryId: 1196, categoryName: '여행'),
+  Cate(categoryId: 13789, categoryName: '유아'),
+  Cate(categoryId: 1108, categoryName: '어린이'),
+  Cate(categoryId: 1137, categoryName: '청소년'),
+  Cate(categoryId: 2551, categoryName: '만화'),
+  Cate(categoryId: 2105, categoryName: '고전'),
+  Cate(categoryId: 1383, categoryName: '자격증/수험'),
+  Cate(categoryId: 1322, categoryName: '외국어'),
+  Cate(categoryId: 8257, categoryName: '대학교재/전문서적'),
+];
