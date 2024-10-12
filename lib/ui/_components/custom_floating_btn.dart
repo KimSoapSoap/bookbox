520f5a7c3fd93613e81865eb3f6a20c3916b384a
kimsoapsoap <zerfa@naver.com>
2024-10-12 20:04:13 +0900

    feat: 대여화면 플로í팅 기ë버튼 추가

--- lib/ui/_components/custom_floating_btn.dart
+++ lib/ui/_components/custom_floating_btn.dart
@@ -1,28 +1,47 @@
 import 'package:flutter/material.dart';
 
 class CustomFloatingButton extends StatelessWidget {
-  final List<ListTile> actions; // ListTile 리스트로 받기
+  final List<ListTile> actions;
+  final double width;
+  final double height;
+  final Color backgroundColor;
+  final double opacity; // 투명도 설정 -> 기본 0.3
+  final double borderRadius; // 모서리 둥글기 추가
+  final Widget icon; // 아이콘을 커스터마이징할 수 있도록 필드 추가
 
   CustomFloatingButton({
-    required this.actions, // 가변 인자처럼 사용 가능
+    required this.actions,
+    this.width = 30.0,
+    this.height = 30.0,
+    this.backgroundColor = const Color(0xFF7AB6FA),
+    this.opacity = 0.3,
+    this.borderRadius = 8.0, // 기본값으로 약간 둥글게 설정
+    this.icon = const Icon(Icons.more_vert), // 기본 아이콘 설정
   });
 
   @override
   Widget build(BuildContext context) {
-    return FloatingActionButton(
-      mini: true,
-      onPressed: () {
-        showModalBottomSheet(
-          context: context,
-          builder: (context) {
-            return Wrap(
-              children: actions, // 전달된 ListTile 리스트를 그대로 사용
-            );
-          },
-        );
-      },
-      backgroundColor: Colors.blue,
-      child: Icon(Icons.more_vert),
+    return SizedBox(
+      width: width,
+      height: height,
+      child: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+            useSafeArea: true, // 안전 영역을 고려
+            context: context,
+            builder: (context) {
+              return Wrap(
+                children: actions,
+              );
+            },
+          );
+        },
+        backgroundColor: backgroundColor.withOpacity(opacity),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(borderRadius), // 모서리 반경 설정
+        ),
+        child: Icon(Icons.more_vert),
+      ),
     );
   }
 }
--- lib/ui/main/library/lend_tab/lend_tab.dart
+++ lib/ui/main/library/lend_tab/lend_tab.dart
@@ -1,9 +1,8 @@
-import 'package:bookbox/core/constants/color.dart';
 import 'package:bookbox/core/constants/size.dart';
 import 'package:bookbox/core/constants/styles.dart';
-import 'package:bookbox/ui/_components/custom_action_btn.dart';
 import 'package:bookbox/ui/_components/custom_card_item.dart';
 import 'package:bookbox/ui/_components/custom_dialog.dart';
+import 'package:bookbox/ui/_components/custom_floating_btn.dart';
 import 'package:flutter/material.dart';
 
 class LendTab extends StatelessWidget {
@@ -26,58 +25,72 @@ class LendTab extends StatelessWidget {
             // 그리드의 높이를 내용에 맞게 조절
             gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
               crossAxisSpacing: 25,
-              crossAxisCount: 2,
+              crossAxisCount: 3,
               mainAxisSpacing: 20,
               childAspectRatio: (itemWidth / itemHeight),
             ),
             itemCount: 12,
             itemBuilder: (context, index) {
-              return Column(children: [
-                CustomCardItem(
-                  imageUrl:
-                      "https://picsum.photos/id/${index + 10}/200/280", // 이미지 URL
-                  title: "책 제목\n2줄일 때", // 책 제
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    CustomActionButton(
-                        text: '반납',
-                        onPressed: () {
-                          CustomDialog(
-                              title: '반납 확인',
-                              content: '$index번 책을 반납 하시겠습니까?',
-                              onConfirm: () => {
-                                    print("$index번 책 확인 선택시 실행 로직"),
-                                  }).show(context);
-                        }),
-                    CustomActionButton(
-                        text: '연장',
-                        backgroundColor: PRIMARY_COLOR,
-                        onPressed: () {
-                          CustomDialog(
-                              title: '연장 확인',
-                              content: '$index번 책을 연장 하시겠습니까?',
-                              onConfirm: () => {
-                                    print("$index번 책 확인 선택시 실행 로직"),
-                                  }).show(context);
-                        }),
-                    CustomActionButton(
-                        text: '정보',
-                        textColor: PRIMARY_COLOR,
-                        backgroundColor: Colors.white38,
-                        onPressed: () {
-                          //상세 페이지 이동 로직
-                          print('상세페이지 드가자');
-                        }),
-                  ],
-                ),
-                SizedBox(height: 5),
-                Text(
-                  '기한: 24-10-20',
-                  style: TextStyle(fontSize: 12),
-                ),
-              ]);
+              return Stack(
+                children: [
+                  InkWell(
+                    onTap: () {
+                      print('책 클릭 $index');
+                    },
+                    child: Column(children: [
+                      CustomCardItem(
+                        imageUrl:
+                            "https://picsum.photos/id/${index + 10}/200/280",
+                        // 이미지 URL
+                        title: "책 제목\n2줄일 때", // 책 제
+                      ),
+                      Text(
+                        '기한: 24-10-20',
+                        style: TextStyle(fontSize: 12),
+                      ),
+                    ]),
+                  ),
+                  Positioned(
+                    right: -5,
+                    top: 0,
+                    child: CustomFloatingButton(
+                      actions: [
+                        ListTile(title: Text('책 이름 $index')),
+                        ListTile(
+                            title: Text('정보'),
+                            onTap: () {
+                              Navigator.pop(context);
+                              print('책 상세정보로 이동');
+                            }),
+                        ListTile(
+                          title: Text('연장'),
+                          onTap: () {
+                            Navigator.pop(context); //하단 모달창 닫아주려고 씀
+                            CustomDialog(
+                                title: '연장 확인',
+                                content: '$index번 책을 연장 하시겠습니까?',
+                                onConfirm: () => {
+                                      print("$index번 책 확인 선택시 연장 로직 실행"),
+                                    }).show(context);
+                          },
+                        ),
+                        ListTile(
+                          title: Text('반납'),
+                          onTap: () {
+                            Navigator.pop(context);
+                            CustomDialog(
+                                title: '반납 확인',
+                                content: '$index번 책을 반납 하시겠습니까?',
+                                onConfirm: () => {
+                                      print("$index번 책 확인 선택시 반납 로직 실행"),
+                                    }).show(context);
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              );
             },
           ),
         ],
