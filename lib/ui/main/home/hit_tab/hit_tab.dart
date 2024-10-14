6b7c1cb41cc3fec888c59fadbf5c4e8b0c7e59f5
mouse <hyeji111544@gmail.com>
2024-10-14 16:11:02 +0900

    feat : 인기 페이지

--- lib/ui/main/home/hit_tab/hit_tab.dart
+++ lib/ui/main/home/hit_tab/hit_tab.dart
@@ -1,15 +1,37 @@
+import 'dart:ui';
+import 'package:bookbox/core/constants/size.dart';
+import 'package:bookbox/core/constants/styles.dart';
+import 'package:bookbox/ui/_components/custom_list_item.dart';
+import 'package:bookbox/ui/main/home/cate_tab/cate_tab_vm.dart';
 import 'package:flutter/material.dart';
 
 class HitTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue[100],
-      child: Center(
-        child: Text(
-          '내서재',
-          style: TextStyle(fontSize: 20, color: Colors.black),
-        ),
+    TextTheme theme = lightTextTheme();
+
+    return Padding(
+      padding: const EdgeInsets.all(gap_s),
+      child: Stack(
+        children: [
+          ListView(
+            children: [
+              Text(
+                '✨인기 책들을 만나 보세요!',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 16,
+                ),
+              ),
+              ...BookList.map((book) {
+                return CustomListItem(
+                  theme: theme,
+                  book: book,
+                );
+              }).toList(),
+            ],
+          ),
+        ],
       ),
     );
   }
